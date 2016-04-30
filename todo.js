9f002e930e2d74cea5c787e29ef004539df59b11
Felipe Skinner <felipeskinner@gmail.com>
2016-04-30 20:13:29 -0300

    Basic example

---
+++ todo.js
@@ -0,0 +1,13 @@
+const _input = $('.input');
+const _todos = $('.todos');
+
+const keyup$ = Rx.Observable.fromEvent(_input, 'keyup');
+
+const todoItem$ = keyup$
+  .filter(ev => ev.keyCode === 13)
+  .map(ev => ev.target.value);
+
+todoItem$.subscribe(data => {
+  $(`<li>${data}</li>`).appendTo(_todos);
+  $('.input').val(''); //change it
+});
