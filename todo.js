2b7161b1089866eb054d7a7694e4ab03379648ab
Felipe Skinner <felipeskinner@gmail.com>
2016-05-01 11:54:15 -0300

    Improve list display by merging and scanning streams

--- todo.js
+++ todo.js
@@ -3,12 +3,20 @@ const _todos = $('.todos');
 
 const keyup$ = Rx.Observable.fromEvent(_input, 'keyup');
 
+const todos = ['Study RxJS'];
+
 const todoItem$ = keyup$
   .filter(ev => ev.keyCode === 13)
   .map(ev => ev.target.value)
   .filter(msg => msg.trim().length);
 
-todoItem$.subscribe(data => {
-  $(`<li>${data}</li>`).appendTo(_todos);
+const todos$ = Rx.Observable.of(todos)
+  .merge(todoItem$)
+  .scan((list, item) => [ ... list, item ])
+
+todos$.subscribe(data => {
+  _todos.empty();
+
+  data.map(item => $(`<li>${item}</li>`).appendTo(_todos))
   $('.input').val(''); //change it
 });
